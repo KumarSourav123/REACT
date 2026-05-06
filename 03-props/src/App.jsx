0ce84e690961d5c404192a672438d2d8e66acbe8
Kumar Sourav <kumarsourav.net120@gmail.com>
2026-05-07 01:47:38 +0530

    Completed props in React

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,120 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react'
+import Card from "./components/card.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const jobOpenings = [
+  {
+    brandLogo: "https://media.wired.com/photos/5926ffe47034dc5f91bed4e8/3:2/w_2560%2Cc_limit/google-logo.jpg",
+    companyName: "Google",
+    datePosted: "3 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://i0.wp.com/www.printmag.com/wp-content/uploads/2012/09/2a34d8_0780274467a2423dad8f53021b21caadmv2.png?fit=376%2C375&quality=80&ssl=1",
+    companyName: "Microsoft",
+    datePosted: "1 week ago",
+    post: "React Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$50/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5rIxFRNX2QZlnQidGjE75yAg9jR4Jhy3Jgw&s",
+    companyName: "Apple",
+    datePosted: "5 days ago",
+    post: "UI Engineer",
+    tag1: "Part Time",
+    tag2: "Senior Level",
+    pay: "$70/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://static.vecteezy.com/system/resources/thumbnails/014/018/561/small/amazon-logo-on-transparent-background-free-vector.jpg",
+    companyName: "Amazon",
+    datePosted: "2 weeks ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$48/hr",
+    location: "Chennai, India"
+  },
+  {
+    brandLogo: "https://img.freepik.com/premium-vector/meta-company-logo_265339-667.jpg?semt=ais_hybrid&w=740&q=80",
+    companyName: "Meta",
+    datePosted: "4 days ago",
+    post: "Frontend Engineer",
+    tag1: "Remote",
+    tag2: "Senior Level",
+    pay: "$75/hr",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    companyName: "Netflix",
+    datePosted: "10 days ago",
+    post: "JavaScript Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$80/hr",
+    location: "Delhi, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzTmVc7unwsynm3Ofz31vehWOoGK4FMpkPtw&s",
+    companyName: "Adobe",
+    datePosted: "6 days ago",
+    post: "Frontend Intern",
+    tag1: "Internship",
+    tag2: "Entry Level",
+    pay: "$25/hr",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnM8U7Fu9UbOMCakFdEen8a8sl_EavE10XHw&s",
+    companyName: "Uber",
+    datePosted: "3 weeks ago",
+    post: "Web Developer",
+    tag1: "Contract",
+    tag2: "Mid Level",
+    pay: "$55/hr",
+    location: "Gurgaon, India"
+  },
+  {
+    brandLogo: "https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/nvidia-og-image-white-bg-1200x630.jpg",
+    companyName: "NVIDIA",
+    datePosted: "2 days ago",
+    post: "UI/UX Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$68/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCGQHwyoYbOZNE0GLZaUOcTpsd0Zi_XnGcuA&s",
+    companyName: "Salesforce",
+    datePosted: "8 days ago",
+    post: "Frontend Engineer",
+    tag1: "Remote",
+    tag2: "Junior Level",
+    pay: "$52/hr",
+    location: "Mumbai, India"
+  }
+];
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="parent">
 
-      <div className="ticks"></div>
+      {jobOpenings.map(function(elem,idx){
+        return <div key={idx}>
+          <Card key={idx} companyName={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} tag2={elem.tag2} brandLogo={elem.brandLogo} pay={elem.pay} location={elem.location} />
+          </div>
+      })}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    </div>
   )
 }
 
